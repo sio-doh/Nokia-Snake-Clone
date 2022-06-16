df6d2329e91758e1bc22ff79327cff55b674e411
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-06-16 14:18:37 +0200

    fourth commit

--- input.js
+++ input.js
@@ -10,7 +10,7 @@ export function getInputDir() {
 let top = document.getElementById('top');
 let down = document.getElementById('down');
 let left = document.getElementById('left');
-let right = document.getElementById('right');
+let right = document.getElementById('right'); 
 
 // execute function when user presses a key on keyboard
 top.addEventListener("click", function() { 
@@ -35,31 +35,31 @@ right.addEventListener("click", function() {
     if ('right'.key === "Enter") {
         document.getElementById('right').click();
     } 
-});
+}); 
 
 window.addEventListener('keydown', e => {
     // e.key is key up, key down, key left or key right
     switch (e.key) {
-        case 'ArrowUp': 
+        case 'ArrowUp' || top: 
             // if moving up or down at present ignore line 48
             // cannot select up if direction is going down 
             if (lastInputDir.y !== 0) break 
             // y: -1 moves up
             inputDir = { x: 0, y: -1 } 
             break            
-        case 'ArrowDown': 
+        case 'ArrowDown' || down: 
             // if moving up or down at present ignore line 54
             if (lastInputDir.y !== 0) break
             // y: 1 moves down
             inputDir = { x: 0, y: 1 } 
             break  
-        case 'ArrowLeft': 
+        case 'ArrowLeft' || left: 
             // if moving left or right at present ignore line 60 
             if (lastInputDir.x !== 0) break 
             // x: -1 moves left
             inputDir = { x: -1, y: 0 } 
             break  
-        case 'ArrowRight': 
+        case 'ArrowRight' || right: 
             // if moving left or right at present ignore line 66
             if (lastInputDir.x !== 0) break 
             // x: 1 moves right
